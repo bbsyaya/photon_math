aab2990237b4b18c34f89789b6a4129958b365a3
prashantrar <prashantrar@gmail.com>
2016-02-19 18:02:11 +0800

    updating according to conformation

--- firmware/photon_math.cpp
+++ firmware/photon_math.cpp
@@ -1,1 +1,14 @@
 //This file is not used, for function definitions refer photon_math.h
+#include "photon_math.h"
+
+
+int photon_math::add_nos(int a[], int size)
+{
+int ret_sum,i;
+ret_sum = i = 0;
+for(i=0;i<size;i++)
+{
+ret_sum += a[i];
+}
+return ret_sum;
+}
