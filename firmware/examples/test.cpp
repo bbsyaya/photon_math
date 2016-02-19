b7710b698e68aedee442c189886eee54d6e1db51
prashantrar <prashantrar@gmail.com>
2016-02-19 18:04:04 +0800

    updating class names

--- firmware/examples/test.cpp
+++ firmware/examples/test.cpp
@@ -5,11 +5,11 @@ int main()
 {
 int retsum,i;
 retsum = i = 0;
-math_add add_n;
+photon_math math_n;
 
 int a[]={1,2,3,4,5,6,7,8,9};
 
-retsum = add_n.add_nos(a , 9);
+retsum = math_n.add_nos(a , 9);
 
 cout<<"The sum is"<<retsum<<endl;
 
